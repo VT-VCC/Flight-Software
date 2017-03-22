c9c607da412ebfc3095b3f89d400e71e81c4fcaf
bobtwinkles <srkoser+github@gmail.com>
2017-03-22 16:56:57 -0400

    Rework the build system again
    This converts the build system to be one big CMake project.
    Read the build.sh script in order to see how to build the various MSP430
    targets. By default, we build a single test binary called "usip_test" which
    runs all the collected test code. To build specific targets you must define
    at least one of
      - USIP_DATA_BOARD
      - USIP_DEV_BOARD
      - USIP_SENSOR_BOAD
    to be true using -D${variable}=TRUE.
    In addition, you must provide the MSP430 toolchain:
      -DCMAKE_TOOLCHAIN_FILE=path/to/cmake/custom_toolchains/msp430.cmake
    and tell CMake what board you're building for
      -DMSP430_MCU=msp430fr5849

--- build.sh
+++ build.sh
@@ -3,9 +3,34 @@ set -euo pipefail
 
 cd $(dirname $0)
 
-for i in dev_board sensor_board data_board
+mkdir -p build-host
+cd build-host
+cmake ../
+make
+cd ../
+
+for i in data-board dev-board sensor-board
 do
-  cd $i
-  ./build.sh
+  mkdir -p build-$i
+  cd build-$i
+  case $i in
+    data-board)
+      cmake_args='-DUSIP_DATA_BOARD=TRUE -DMSP430_MCU=msp430f5438a'
+      ;;
+    dev-board)
+      cmake_args='-DUSIP_DEV_BOARD=TRUE -DMSP430_MCU=msp430fr5969'
+      ;;
+    sensor-board)
+      cmake_args='-DUSIP_SENSOR_BOARD=TRUE -DMSP430_MCU=msp430fr5849'
+  esac
+  toolchain_file=$(pwd)/../cmake/custom_toolchains/msp430.cmake
+  echo
+  echo "********************************************************************************"
+  echo "    Building using toolchain ${toolchain_file}"
+  echo "********************************************************************************"
+  echo
+
+  cmake -DCMAKE_TOOLCHAIN_FILE=${toolchain_file} ${cmake_args} ../
+  make
   cd ../
 done
