61b7a4145120aa9be6dc0cba85ba2858c604ca91
Nick Miller <nmiller1@vt.edu>
2017-03-25 15:09:15 -0400

    Switched to new build system, did more stuff

--- sensor_board/test/test_pinav_parser.cpp
+++ sensor_board/test/test_pinav_parser.cpp
@@ -3,26 +3,28 @@
 #include "pinav_parser.h"
 
 // @TODO: incomplete
-TEST_CASE("Valid GGA sentence", "[pinav]") {
+TEST_CASE("Valid GGA sentence", "[pinav_parser]") {
 	pinav_parse_output_t output;
-	char * sentence = "$GPGGA,172120.384,5219.0671,N,05117.0926,E,\r\n";
+	char * sentence = "$GPGGA,172120.384,5219.0671,N,05117.0926,E,1,9,\r\n";
 	pinav_parser_status_t result = parse_pinav_sentence(&output, (uint8_t *)sentence);
 	REQUIRE(result == UNKNOWN_ERROR);
 	REQUIRE(output.id == GGA);
 	REQUIRE(output.data.gga.fix_time_millis == 62480384);
 	REQUIRE(output.data.gga.latitude == 52317785);
 	REQUIRE(output.data.gga.longitude == 51284876);
+	REQUIRE(output.data.gga.fix_quality == FIX_VALID);
+	REQUIRE(output.data.gga.sat_count == 9);
 }
 
 TEST_CASE("Null output pointer", "[pinav_parser]"){
 	pinav_parser_status_t result = parse_pinav_sentence(nullptr, nullptr);
-	REQUIRE(result == NULL_OUTPUT_PTR);
+	REQUIRE(result == PN_PARSE_NULL_OUTPUT_PTR);
 }
 
 TEST_CASE("Null sentence pointer", "[pinav_parser]"){
 	pinav_parse_output_t output;
 	pinav_parser_status_t result = parse_pinav_sentence(&output, nullptr);
-	REQUIRE(result == NULL_SENTENCE_PTR);
+	REQUIRE(result == PN_PARSE_NULL_SENTENCE_PTR);
 	REQUIRE(output.id == NONE);
 }
 
@@ -30,7 +32,7 @@ TEST_CASE("Sentence doesn't start with $" "[pinav_parser]") {
 	pinav_parse_output_t output;
 	char * sentence = "ABCD\r\n";
 	pinav_parser_status_t result = parse_pinav_sentence(&output, (uint8_t *) sentence);
-	REQUIRE(result == SENTENCE_FORMAT_ERROR);
+	REQUIRE(result == PN_PARSE_SENTENCE_FORMAT_ERROR);
 	REQUIRE(output.id == NONE);
 }
 
@@ -42,7 +44,7 @@ TEST_CASE("Invalid sentence length", "[pinav_parser]") {
 		sentence[i] = 'a';
 	}
 	pinav_parser_status_t result = parse_pinav_sentence(&output, sentence);
-	REQUIRE(result == IMPROPER_SENTENCE_LENGTH);
+	REQUIRE(result == PN_PARSE_IMPROPER_SENTENCE_LENGTH);
 	REQUIRE(output.id == NONE);
 }
 
@@ -50,6 +52,6 @@ TEST_CASE("Invalid sentence type", "[pinav_parser]") {
 	pinav_parse_output_t output;
 	char * sentence = "$GPABC,\r\n";
 	pinav_parser_status_t result = parse_pinav_sentence(&output, (uint8_t *)sentence);
-	REQUIRE(result == UNRECOGNIZED_SENTENCE_TYPE);
+	REQUIRE(result == PN_PARSE_UNRECOGNIZED_SENTENCE_TYPE);
 	REQUIRE(output.id == NONE);
 }
