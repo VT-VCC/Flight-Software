edf5c6d121843fec72438b4664272d5ab62ed709
Nick Miller <nmiller1@vt.edu>
2017-04-08 15:16:48 -0400

    Successfully parsing GGA and LSP sentences

--- sensor_board/test/test_pinav_parser.cpp
+++ sensor_board/test/test_pinav_parser.cpp
@@ -1,19 +1,51 @@
 #include <catch/catch.hpp>
-
 #include "pinav_parser.h"
 
 // @TODO: incomplete
 TEST_CASE("Valid GGA sentence", "[pinav_parser]") {
 	pinav_parse_output_t output;
-	char * sentence = "$GPGGA,172120.384,5219.0671,N,05117.0926,E,1,9,\r\n";
+	char * sentence = "$GPGGA,172120.384,5219.0671,N,05117.0926,E,1,9,0.9,371262.1,M,0,M,,,*54\r\n";
 	pinav_parser_status_t result = parse_pinav_sentence(&output, (uint8_t *)sentence);
-	REQUIRE(result == UNKNOWN_ERROR);
+	REQUIRE(result == PN_PARSE_OK);
 	REQUIRE(output.id == GGA);
 	REQUIRE(output.data.gga.fix_time_millis == 62480384);
 	REQUIRE(output.data.gga.latitude == 52317785);
 	REQUIRE(output.data.gga.longitude == 51284876);
 	REQUIRE(output.data.gga.fix_quality == FIX_VALID);
 	REQUIRE(output.data.gga.sat_count == 9);
+	REQUIRE(output.data.gga.hdop == 0b0000000011100110); //0.9 in 8.8 fixed-point
+	REQUIRE(((output.data.gga.altitude >= 37126209) && (output.data.gga.altitude <= 37126211))); // Some leniency for binary/decimal conversion error
+}
+
+TEST_CASE("Valid LSP sentence", "[pinav_parser]") {
+	pinav_parse_output_t output;
+	char * sentence = "$PSLSP,193772.250,780,3963889.500,1001383.750,4879428.125,5,4.5*72\r\n";
+	pinav_parser_status_t result = parse_pinav_sentence(&output, (uint8_t *)sentence);
+	REQUIRE(result == PN_PARSE_OK);
+	REQUIRE(output.id == LSP);
+	int64_t expected_val = 193772;
+	expected_val <<= 32;
+	uint32_t fraction = 0b01000000 << 24;
+	expected_val |= fraction;
+	REQUIRE(output.data.lsp.gps_time_seconds == expected_val);
+	REQUIRE(output.data.lsp.gps_week == 780);
+	expected_val = 3963889;
+	expected_val <<= 32;
+	fraction = 0b10000000 << 24;
+	expected_val |= fraction;
+	REQUIRE(output.data.lsp.wgs_x == expected_val);
+	expected_val = 1001383;
+	expected_val <<= 32;
+	fraction = 0b11000000 << 24;
+	expected_val |= fraction;
+	REQUIRE(output.data.lsp.wgs_y == expected_val);
+	expected_val = 4879428;
+	expected_val <<= 32;
+	fraction = 0b00100000 << 24;
+	expected_val |= fraction;
+	REQUIRE(output.data.lsp.wgs_z == expected_val);
+	REQUIRE(output.data.lsp.sat_count == 5);
+	REQUIRE(output.data.lsp.pdop == 0x0480);
 }
 
 TEST_CASE("Null output pointer", "[pinav_parser]"){
