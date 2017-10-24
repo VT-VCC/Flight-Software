208490246750e620436a99ebe29fde3f8770dd4c
Akhil Nagdev <akhil94@vt.edu>
2017-10-23 20:00:49 -0400

    Added I2C header files and function stubs

---
+++ board_common/common/i2c.c
@@ -0,0 +1,23 @@
+#include "i2c.h"
+/******************************************************************************\
+ *  Public interface implementations                                          *
+\******************************************************************************/
+void i2c_write_string() {};
+
+void i2c_read_string() {};
+
+void i2c_write_bytes() {};
+
+void i2c_read_bytes() {};
+
+#ifndef NDEBUG
+const char * i2c_error_string(i2c_error_t t) {
+    switch(t) {
+#       define STRING_OP(E) case I2C_ ## E: return #E;
+        I2C_ERROR_LIST(STRING_OP)
+#       undef STRING_OP
+        default:
+            return "I2C error unknown";
+    }
+}
+#endif
--- clean_builds.sh
+++ clean_builds.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find -path '*/build-*/*' -delete
+find ./ -path '*/build-*/*' -delete
