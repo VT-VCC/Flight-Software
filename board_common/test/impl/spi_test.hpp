88caaf544112e4bb40efbbd02db1450d6dc754b0
Andrew Dudash <drewd8@vt.edu>
2017-10-28 22:58:26 +0000

    Start writing tests and test stub for SPI.

--- board_common/test/impl/spi_test.hpp
+++ board_common/test/impl/spi_test.hpp
@@ -19,6 +19,25 @@ extern "C" {
 #include <catch/catch.hpp>
 #include <vector>
 
+struct spi_impl {
+  std::vector<uint8_t> miso_bytes;
+  std::vector<uint8_t> mosi_bytes;
+  bool open;
+
+  void send_byte();
+
+  spi_impl() : open(false) {}
+};
+
+void spi_open(spi_t * out);
+void spi_close(spi_t * out);
+spi_error_t spi_send_byte(spi_t * channel, uint8_t byte);
+spi_error_t spi_receive_byte(spi_t * channel, uint8_t & byte);
+spi_transfer_byte(spi_t * channel, uint8_t send_byte, uint8_t * receive_byte);
+
+std::ostream & operator<<(std::ostream & o, const spi_t & t);
+std::ostream & operator<<(std::ostream & o, const spi_error_t & err);
+
 #endif
 
 #endif // _TEST_SPI_HPP_
--- board_common/test/spi.cpp
+++ board_common/test/spi.cpp
@@ -2,6 +2,20 @@
 
 #include "spi.h"
 
-TEST_CASE("Test SPI implementation can push bytes", "[spi]") {
-	//TODO! Add tests
+TEST_CASE("Test SPI interface with single bytes on stub that increments bytes.", "[spi]") {
+    spi_t t;
+    spi_error_t error;
+
+    // SPI Device that just increments
+    spi_send(&t, &error, 1)
+    REQUIRE(spi_receive(&t, &error) == 2);
+    spi_send(&t, &error, 10);
+    REQUIRE(spi_receive(7t, &error) == 11);
+}
+
+TEST_CASE("Test SPI interface with bulk data on stub that increments bytes.", "[spi]") {
+    spi_t t;
+    spi_error_t error;
+    uint8_t test_data[] = { 5, 10, 20 }
+
 }
