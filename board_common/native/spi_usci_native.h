40d74ff2bbfdeb6b6cd29f630048d17bc47b1fa0
Brandon Walker <brandon@branw.xyz>
2017-11-19 20:31:05 -0500

    usci/eusci: Move common definitions to separate headers

--- board_common/native/spi_usci_native.h
+++ board_common/native/spi_usci_native.h
@@ -1,51 +1,11 @@
 #ifndef _BOARD_COMMON_NATIVE_SPI_USCI_H_
 #define _BOARD_COMMON_NATIVE_SPI_USCI_H_
 
+#include "usci_native.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-    
-    // TODO: This shuld probably be extracted to a common interface
-#include <msp430.h>
-#if defined (MSP430_CLASS_F5xx_6xx)
-#   include "msp430f5xx_6xxgeneric.h"
-#else
-#   error "Unsupported processor"
-#endif
-    
-#ifndef __MSP430_HAS_USCI_Ax__
-#   error "We need USCI for SPI"
-#endif
-    
-#include <driverlib.h>
-
-typedef enum usci {
-#ifdef USCI_A0_BASE
-    USCI_A0,
-#endif
-#ifdef USCI_A1_BASE
-    USCI_A1,
-#endif
-#ifdef USCI_A2_BASE
-    USCI_A2,
-#endif
-#ifdef USCI_A3_BASE
-    USCI_A3,
-#endif
-#ifdef USCI_B0_BASE
-    USCI_B0,
-#endif
-#ifdef USCI_B1_BASE
-    USCI_B1,
-#endif
-#ifdef USCI_B2_BASE
-    USCI_B2,
-#endif
-#ifdef USCI_B3_BASE
-    USCI_B3,
-#endif
-    USCI_count
-} usci_t;
 
 typedef struct spi {
     /**
