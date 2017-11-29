cd2dea4fa40150f5a85be621c56ae2d94e696ac5
Andrew Dudash <drewd8@vt.edu>
2017-11-29 16:27:24 -0500

    Add some more SPI tests and buff up existing ones.

--- board_common/test/spi.cpp
+++ board_common/test/spi.cpp
@@ -8,7 +8,10 @@ TEST_CASE("Test SPI interface with single bytes on stub that increments bytes.",
   uint8_t receive = 0;
   spi_open(&t);
   REQUIRE(spi_transfer_byte(&t, send, &receive) == SPI_NO_ERROR);
-
+  REQUIRE(receive == send + 1);
+  REQUIRE_THAT(t, HasMasterOutSlaveInBytes({ 0x03 }));
+  REQUIRE_THAT(t, HasMasterInSlaveOutBytes({ 0x04 }));
+	       
   spi_close(&t);
 }
 
@@ -35,4 +38,51 @@ TEST_CASE("Test SPI interface with closed channel.", "[spi]") {
   REQUIRE_THAT(t, HasMasterOutSlaveInBytes({}));
   REQUIRE_THAT(t, HasMasterInSlaveOutBytes({}));
   spi_close(&t);
+}
+
+TEST_CASE("Test SPI send byte and ignore.", "[spi]") {
+  spi_t t;
+  uint8_t send = 0x10;
+
+  spi_open(&t);
+  REQUIRE(spi_send_byte(&t, 0x10) == SPI_NO_ERROR);
+  REQUIRE_THAT(t, HasMasterOutSlaveInBytes({ 0x10 }));
+  REQUIRE_THAT(t, HasMasterInSlaveOutBytes({ 0x11 }));
+  spi_close(&t);
+}
+
+TEST_CASE("Test SPI send junk and receive byte.", "[spi]") {
+  spi_t t;
+  uint8_t receive = 0;
+  
+  spi_open(&t);
+  REQUIRE(spi_receive_byte(&t, &receive) == SPI_NO_ERROR);
+  spi_close(&t);
+}
+
+TEST_CASE("Test SPI send bytes and ignore.", "[spi]") {
+  spi_t t;
+  uint8_t send[] = { 0x10, 0x20, 0x30 };
+
+  spi_open(&t);
+  REQUIRE(spi_send_bytes(&t, send, 3) == SPI_NO_ERROR);
+  REQUIRE_THAT(t, HasMasterOutSlaveInBytes({ 0x10, 0x20, 0x30 }));
+  REQUIRE_THAT(t, HasMasterInSlaveOutBytes({ 0x11, 0x21, 0x31}));
+  spi_close(&t);
+}
+
+TEST_CASE("Test SPI send junk and receive bytes.", "[spi]") {
+  spi_t t;
+  uint8_t receive[3];
+  
+  spi_open(&t);
+  REQUIRE(spi_receive_bytes(&t, receive, 3) == SPI_NO_ERROR);
+  spi_close(&t);
+}
+
+TEST_CASE("Test error on multi-byte function.", "[spi]") {
+  spi_t t;
+  uint8_t receive[3];
+
+  REQUIRE(spi_receive_bytes(&t, receive, 3) == SPI_CHANNEL_CLOSED);
 }
