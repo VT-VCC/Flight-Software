b793ba616291c8a4b9126c566dba5f3dc6daf26f
HarounAns <hansari97@gmail.com>
2018-02-19 17:15:28 -0500

    changes regarding return values of read and write

--- board_common/common/eps.c
+++ board_common/common/eps.c
@@ -6,68 +6,70 @@
 /******************************************************************************\
  *  Private support functions                                                 *
 \******************************************************************************/
- eps_t eps_read(i2_t * channel, );
+ eps_t eps_read(ep_t * channel, uint8_t * data[1]);
  eps_result_t eps_write(uint8_t * command, uint8_t * data_parameter);
 
  /******************************************************************************\
  *  Private support function implementations                                  *
  \******************************************************************************/
 
- eps_t eps_read(ep_t * channel, uint8_t * data[]) {
+ eps_t eps_read(ep_t * channel, uint8_t * data[1]) {
 	 uint8_t slave_address = 0x2B;
+	 uint8_t * data[1];
 	 
 	 //Check if byte matches slave
-	 i2c_error_t readErr1 i2c_read_byte(channel, value);
-	 if (value != slave_address || readErr1 != I2C_NO_ERROR) {
+	 i2c_error_t err i2c_read_byte(channel, value);
+	 if (value != slave_address || err != I2C_NO_ERROR) {
 		 return EPS_BAD_COMMUNICATION;
 	 }
 
-	 //get data[0]
-	 i2c_error_t readErr2 i2c_read_byte(channel, value);
-	 if (readErr2 != I2C_NO_ERROR) {
-		 return EPS_BAD_COMMUNICATION;
-	 }
-	 data[0] = value;
-
-	 //get data[1]
-	 i2c_error_t readErr3 i2c_read_byte(channel, value);
-	 if (readErr3 != I2C_NO_ERROR) {
+	 //get data[1] the MSB
+	 i2c_error_t err i2c_read_byte(channel, value);
+	 if (err != I2C_NO_ERROR) {
 		 return EPS_BAD_COMMUNICATION;
 	 }
 	 data[1] = value;
+
+	 //get data[0] the LSB
+	 i2c_error_t err i2c_read_byte(channel, value);
+	 if (err != I2C_NO_ERROR) {
+		 return EPS_BAD_COMMUNICATION;
+	 }
+	 data[0] = value;
 	 
-	 //not sure what I should return for data array since it stores 2 bytes.
-	 return EPS_NO_ERROR; 
+	 //returns 2 bytes that are read with first byte as MSB and 2nd byte as LSB
+	 return (data[1] << 8) | data[0] ; 
  }
 
  eps_result_t eps_write(ep_t * channel, uint8_t * command, uint8_t * data_parameter) {
 	 uint8_t slave_address = 0x2B;
 
 	 // Write Slave Address via I2C
-	 i2c_error_t writeErr1 = i2c_write_byte(channel, slave_address);
-	 if (err1 != I2C_NO_ERROR) {
+	 i2c_error_t err = i2c_write_byte(channel, slave_address);
+	 if (err != I2C_NO_ERROR) {
 		 return EPS_BAD_COMMUNICATION;
 	 }
 
 	 // Write Command via I2C
-	 i2c_error_t writeErr2 = i2c_write_byte(channel, command);
-	 if (err2 != I2C_NO_ERROR) {
+	 i2c_error_t err = i2c_write_byte(channel, command);
+	 if (err != I2C_NO_ERROR) {
 		 return EPS_BAD_COMMUNICATION;
 	 }
 
 	 // Write Command via I2C
-	 i2c_error_t writeErr3 = i2c_write_byte(channel, data_parameter);
-	 if (err2 != I2C_NO_ERROR) {
+	 i2c_error_t err = i2c_write_byte(channel, data_parameter);
+	 if (err != I2C_NO_ERROR) {
 		 return EPS_BAD_COMMUNICATION;
 	 }
 
 	 //Read Response Bit 
-	 i2c_error_t readErr1 = i2c_read_byte(channel, value);
-	 if (value == 0xFFFF || readErr1 != I2C_NO_ERROR) {
+	 i2c_error_t err = i2c_read_byte(channel, value);
+	 if (value == 0xFFFF || err != I2C_NO_ERROR) {
 		 return EPS_BAD_COMMUNICATION;
 	 }
 	
-	 return EPS_NO_ERROR;
+	 // return the data parameter as the MSB and the command as the LSB
+	 return (data_parameter << 8) | command;
  }
 
 
