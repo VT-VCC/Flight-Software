f41529e49c61e842750845d54722d355cc8be229
HarounAns <hansari97@gmail.com>
2018-04-23 16:38:42 -0400

    Add files via upload

---
+++ eps.c
@@ -0,0 +1,69 @@
+// eps.c
+// Author: Haroun Ansari
+#include "eps.h"
+#include "eps_internal.h"
+
+ /******************************************************************************\
+ *  Private support function implementations                                  *
+ \******************************************************************************/
+
+ eps_t eps_read(i2c_t * channel) {
+	 uint8_t slave_address = 0x2B;
+	 uint8_t * data[1];
+	 
+	 //Check if byte matches slave
+	 i2c_error_t err i2c_read_byte(channel, value);
+	 if (value != slave_address || err != I2C_NO_ERROR) {
+		 return EPS_BAD_COMMUNICATION;
+	 }
+
+	 //get data[1] the MSB
+	 i2c_error_t err i2c_read_byte(channel, value);
+	 if (err != I2C_NO_ERROR) {
+		 return EPS_BAD_COMMUNICATION;
+	 }
+	 data[1] = value;
+
+	 //get data[0] the LSB
+	 i2c_error_t err i2c_read_byte(channel, value);
+	 if (err != I2C_NO_ERROR) {
+		 return EPS_BAD_COMMUNICATION;
+	 }
+	 data[0] = value;
+	 
+	 //returns 2 bytes that are read with first byte as MSB and 2nd byte as LSB
+	 return (data[1] << 8) | data[0] ; 
+ }
+
+ eps_result_t eps_write(i2c_t * channel, uint8_t * command, uint8_t * data_parameter) {
+	 uint8_t slave_address = 0x2B;
+
+	 // Write Slave Address via I2C
+	 i2c_error_t err = i2c_write_byte(channel, slave_address);
+	 if (err != I2C_NO_ERROR) {
+		 return EPS_BAD_COMMUNICATION;
+	 }
+
+	 // Write Command via I2C
+	 i2c_error_t err = i2c_write_byte(channel, command);
+	 if (err != I2C_NO_ERROR) {
+		 return EPS_BAD_COMMUNICATION;
+	 }
+
+	 // Write Command via I2C
+	 i2c_error_t err = i2c_write_byte(channel, data_parameter);
+	 if (err != I2C_NO_ERROR) {
+		 return EPS_BAD_COMMUNICATION;
+	 }
+
+	 //Read Response Bit 
+	 i2c_error_t err = i2c_read_byte(channel, value);
+	 if (value == 0xFFFF || err != I2C_NO_ERROR) {
+		 return EPS_BAD_COMMUNICATION;
+	 }
+	
+	 // return the data parameter as the MSB and the command as the LSB
+	 return (data_parameter << 8) | command;
+ }
+
+
---
+++ eps.h
@@ -0,0 +1,68 @@
+ifndef _COMMON_EPS_H_
+#define _COMMON_EPS_H_
+
+#include "i2c.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	/**
+	* Macro list for results of operations on the Lithium
+	*/
+#define EPS_RESULT_LIST(OP) \
+    OP(NO_ERROR) \
+    OP(BAD_COMMUNICATION)
+
+	/**
+	* Enumeration of possible results for trying to communicate with the Lithium
+	*/
+	typedef enum eps_result {
+#   define STRING_OP(E) EPS_ ## E,
+		EPS_RESULT_LIST(STRING_OP)
+#   undef STRING_OP
+		EPS_count
+	} eps_result_t;
+
+
+	/**
+	* Represents a connection to a Lithium radio
+	*/
+	typedef struct eps {
+		/**
+		* The I2C channel over which we talk
+		*/
+		i2c_t i2c;
+	} eps_t;
+
+	/**
+	* EPS write function.
+	*
+	* @param i2c channel that we read from
+	* command byte
+	* data parameter byte
+	*
+	* @return data value that is read from EPS 
+	* if successful and returns EPS_BAD_COMMUNICATION
+	* if unsuccessful
+	*/
+	eps_t eps_write(i2c_t * channel, uint8_t * command, uint8_t * data_parameter);
+
+	/**
+	* EPS read function.
+	*
+	* @param i2c channel that we read from
+	*
+	* @return data value that is read from EPS
+	* if successful and returns EPS_BAD_COMMUNICATION
+	* if unsuccessful
+	*/
+	eps_t eps_read(i2c_t * channel);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _COMMON_LITHIUM_H_
