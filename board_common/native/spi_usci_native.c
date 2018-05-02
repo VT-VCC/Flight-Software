cf51185dac11173a6dc404b78f1e9378849966ef
Brandon Walker <brandon@branw.xyz>
2018-05-02 09:33:26 -0400

    spi: Implement USCI methods

--- board_common/native/spi_usci_native.c
+++ board_common/native/spi_usci_native.c
@@ -1,5 +1,9 @@
 #include "spi.h"
 
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
 #include <string.h>
 #include <assert.h>
 
@@ -29,6 +33,46 @@ static uint16_t BASE_ADDRESSES[USCI_count] = {
     USCI_B3,
 #endif
 };
+
+// Semaphore for locking a task while transmitting
+StaticSemaphore_t spi_semaphore_buffer[USCI_count];
+SemaphoreHandle_t spi_semaphore[USCI_count];
+
+static uint8_t *spi_send_buffer[USCI_count];
+static uint8_t *spi_recv_buffer[USCI_count];
+static size_t spi_buffer_index[USCI_count];
+static size_t spi_buffer_size[USCI_count];
+
+#ifdef USCI_A3_BASE
+__attribute__((interrupt(USCI_A3_VECTOR)))
+void USCI_A3_ISR(void) {
+    switch (__even_in_range(UCA3IV, 4)) {
+        case USCI_UCRXIFG:
+            while (!USCI_A_SPI_getInterruptStatus(USCI_A3_BASE, USCI_A_SPI_TRANSMIT_INTERRUPT));
+
+            bool can_recv = spi_recv_buffer[USCI_A3] != 0,
+                can_send = spi_send_buffer[USCI_A3] != 0;
+
+            // Read the current byte
+            if (can_recv) {
+                spi_recv_buffer[USCI_A3][spi_buffer_index[USCI_A3]] = USCI_A_SPI_receiveData(USCI_A3_BASE);
+            }
+
+            spi_buffer_index[USCI_A3]++;
+            if (spi_buffer_index[USCI_A3] < spi_buffer_size[USCI_A3]) {
+                // Send the next byte
+                USCI_A_SPI_transmitData(USCI_A3_BASE, (can_send ? spi_send_buffer[USCI_A3][spi_buffer_index[USCI_A3]] : 0));
+            }
+            else {
+                // Unblock the transfer
+                BaseType_t higher_task_taken;
+                xSemaphoreGiveFromISR(spi_semaphore[USCI_A3], &higher_task_taken);
+                portYIELD_FROM_ISR(higher_task_taken);
+            }
+            break;
+    }
+}
+#endif
 
 // Is a block's base address that of an A or B block
 static bool is_usci_a_block(uint16_t base_address) {
@@ -134,8 +178,74 @@ void spi_close(spi_t * out) {
     }
 }
 
-spi_error_t spi_transfer_bytes(spi_t * channel, uint8_t * send_bytes, uint8_t * receive_bytes, size_t length) {
-    assert(false);
 
+static spi_error_t usci_a_spi_transfer_bytes(usci_t usci, uint16_t base_address, uint8_t * send_bytes, uint8_t * receive_bytes, size_t length) {
+    // Check if the SPI bus is not enabled
+    bool is_in_reset_state = HWREG16(base_address + OFS_UCAxCTLW0) & UCSWRST;
+    if (is_in_reset_state) {
+        return SPI_CHANNEL_CLOSED;
+    }
+
+    taskENTER_CRITICAL();
+    spi_buffer_index[usci] = 0;
+    spi_buffer_size[usci] = length;
+    spi_send_buffer[usci] = send_bytes;
+    spi_recv_buffer[usci] = receive_bytes;
+    taskEXIT_CRITICAL();
+
+    // Wait for the TX buffer to be ready, and by extension the RX buffer
+    // (transmitting while UCxxIFG & UCTXIFG == 0 is undefined behavior)
+    while(!USCI_A_SPI_getInterruptStatus(base_address, USCI_A_SPI_TRANSMIT_INTERRUPT));
+
+    USCI_A_SPI_transmitData(base_address, (send_bytes == 0 ? 0 : send_bytes[0]));
+
+    // Block until the ISR is fired
+    if (xSemaphoreTake(spi_semaphore[usci], 5) == pdTRUE) {
+        memcpy(receive_bytes, spi_recv_buffer[usci], length);
+        return SPI_NO_ERROR;
+    }
+
+    // The timeout was hit and our transfer was never completed
     return SPI_INCOMPLETE;
+}
+
+static spi_error_t usci_b_spi_transfer_bytes(usci_t usci, uint16_t base_address, uint8_t * send_bytes, uint8_t * receive_bytes, size_t length) {
+    // Check if the SPI bus is not enabled
+    bool is_in_reset_state = HWREG16(base_address + OFS_UCBxCTLW0) & UCSWRST;
+    if (is_in_reset_state) {
+        return SPI_CHANNEL_CLOSED;
+    }
+
+    taskENTER_CRITICAL();
+    spi_buffer_index[usci] = 0;
+    spi_buffer_size[usci] = length;
+    spi_send_buffer[usci] = send_bytes;
+    spi_recv_buffer[usci] = receive_bytes;
+    taskEXIT_CRITICAL();
+
+    // Wait for the TX buffer to be ready, and by extension the RX buffer
+    // (transmitting while UCxxIFG & UCTXIFG == 0 is undefined behavior)
+    while(!USCI_B_SPI_getInterruptStatus(base_address, USCI_B_SPI_TRANSMIT_INTERRUPT));
+
+    USCI_B_SPI_transmitData(base_address, (send_bytes == 0 ? 0 : send_bytes[0]));
+
+    // Block until the ISR is fired
+    if (xSemaphoreTake(spi_semaphore[usci], 5) == pdTRUE) {
+        memcpy(receive_bytes, spi_recv_buffer[usci], length);
+        return SPI_NO_ERROR;
+    }
+
+    // The timeout was hit and our transfer was never completed
+    return SPI_INCOMPLETE;
+}
+
+spi_error_t spi_transfer_bytes(spi_t * channel, uint8_t * send_bytes, uint8_t * receive_bytes, size_t length) {
+    uint16_t base_address = BASE_ADDRESSES[channel->usci];
+
+    if (is_usci_a_block(base_address)) {
+        return usci_a_spi_transfer_bytes(channel->usci, base_address, send_bytes, receive_bytes, length);
+    }
+    else {
+        return usci_b_spi_transfer_bytes(channel->usci, base_address, send_bytes, receive_bytes, length);
+    }
 }
